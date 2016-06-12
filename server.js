5ece4f17f32c692c0d854658fd1bf50f44b9d4cd
Alexander Paterson <alex1234@live.com.au>
2016-06-12 17:41:30 +0800

    host from env

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
-app.listen(HOST, function() {
+app.listen(PORT, HOST, function() {
   console.log('Express server is up on', PORT)
 });
