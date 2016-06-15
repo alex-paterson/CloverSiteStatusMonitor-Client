82f652fdc8aa69b2b6b7d0629b975f18e556fb27
Alexander Paterson <alex1234@live.com.au>
2016-06-15 20:47:35 +0800

    shrunk bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -95,5 +95,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
